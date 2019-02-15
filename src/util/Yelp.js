a9a6a3f5371b2a987198115936f2c25d0f849c4e
Ben Siegfried <siegfriedmedia@gmail.com>
2019-02-15 14:10:37 -0800

    Add yelp apikey, yelp object, and search method

---
+++ src/util/Yelp.js
@@ -0,0 +1,7 @@
+const apiKey = 'Smc4_m816T5jBpzVEawxgYTOPiFWqOwk9E2_FoIArsrWwQc1-Ry-J3brFoWxi3KiV6i6j42EN9IPXXzLwKR4mBETZa5UrdnADT1LQZYPep6wmG8yakiCxIkXSRVnXHYx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch();
+  }
+};
