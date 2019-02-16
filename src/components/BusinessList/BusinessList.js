5f68fb5c4d3f5130bcde78b48aa562399aa30656
Ben Siegfried <siegfriedmedia@gmail.com>
2019-02-16 13:51:30 -0800

    add key to business for map to add to each business

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -8,7 +8,7 @@ class BusinessList extends React.Component {
       <div className="BusinessList">
       {
         this.props.businesses.map(value => {
-          return <Business business={value} />
+          return <Business key={business.id} business={value} />
         })
       }
       </div>
