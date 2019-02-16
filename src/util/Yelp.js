0c665fb47ab6a3aa1046e1a75433c752b5cc1a74
Ben Siegfried <siegfriedmedia@gmail.com>
2019-02-16 13:55:35 -0800

    import Yelp into App

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,6 +1,6 @@
 const apiKey = 'Smc4_m816T5jBpzVEawxgYTOPiFWqOwk9E2_FoIArsrWwQc1-Ry-J3brFoWxi3KiV6i6j42EN9IPXXzLwKR4mBETZa5UrdnADT1LQZYPep6wmG8yakiCxIkXSRVnXHYx';
 
-const yelp = {
+const Yelp = {
   search(term, location, sortBy) {
     return fetch(`https://cors-anywhere.herokuapp.com/
 https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
@@ -31,4 +31,4 @@ https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort
   }
 }
 
-export default yelp;
+export default Yelp;
